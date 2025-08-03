d2b06bde04129334503345ae9d571c6b012b1281
Grant Holle <hollegrant@gmail.com>
2025-08-03 17:22:17 -0500

    Add query conditionals

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Post;
 use Illuminate\Http\Request;
+use Laravel\Scout\Builder;
 
 class SearchController extends Controller
 {
@@ -12,7 +13,16 @@ class SearchController extends Controller
      */
     public function __invoke(Request $request)
     {
-        $posts = Post::search($request->get('q', '*'))
+        $posts = Post::search($request->input('q', '*'))
+            ->when(is_numeric($request->input('created')), function (Builder $query) use ($request) {
+                $query->where('created_at', ['>=', now()->subDays($request->integer('created'))->timestamp]);
+            })
+            ->when(! empty($request->input('exclude')), function (Builder $query) use ($request) {
+                $query->whereNotIn('id', explode(',', $request->input('exclude')));
+            })
+            ->when($request->input('not_title'), function (Builder $query, string $title) {
+                $query->where('title', ['!=', $title]);
+            })
             ->get();
 
         return $posts->load('comments');
